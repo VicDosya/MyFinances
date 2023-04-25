015d6b3baf5d452b9b07a70cbed0945d7e1da269
dosya <vicdosya@gmail.com>
2023-04-25 16:22:29 +0300

    Fix MyFinanceApplication

--- app/src/main/java/com/example/myfinance/data/MyFinanceApplication.kt
+++ app/src/main/java/com/example/myfinance/data/MyFinanceApplication.kt
@@ -1,7 +1,6 @@
 package com.example.myfinance.data
 
 import android.app.Application
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.google.firebase.FirebaseApp
 
 class MyFinanceApplication : Application() {
