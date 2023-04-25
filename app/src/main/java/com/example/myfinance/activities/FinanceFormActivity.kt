741e6bb79780ea612d7900ab66a9f0fedca10931
dosya <vicdosya@gmail.com>
2023-04-25 16:20:33 +0300

    Fix code in FinanceFormActivity

--- app/src/main/java/com/example/myfinance/activities/FinanceFormActivity.kt
+++ app/src/main/java/com/example/myfinance/activities/FinanceFormActivity.kt
@@ -7,18 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Switch
 import com.example.myfinance.R
 import com.example.myfinance.data.MyFinanceModal
+import com.example.myfinance.databinding.ActivityFinanceFormBinding
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.Calendar
 
 class FinanceFormActivity : AppCompatActivity() {
+
+    //Initialize viewBinding
+    private lateinit var binding: ActivityFinanceFormBinding
 
     //Initialize the finances collection (fire store is initialized in MyFinanceApplication)
     private val db = FirebaseFirestore.getInstance()
@@ -26,103 +26,23 @@ class FinanceFormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finance_form)
+        binding = ActivityFinanceFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //Enable the back button in the ActionBar
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //Set the ActionBar title
-        supportActionBar?.title = "Add Financial Amount"
+        //Setup the ActionBar with back button and a title
+        setupActionBar()
 
-        //"Add" button listener to add input data on fire store
-        //Get all view from the layouts
-        val addButton = findViewById<Button>(R.id.add_button)
-        val plusMinusSwitch = findViewById<Switch>(R.id.plus_minus_switch)
-        val amountInput = findViewById<EditText>(R.id.amount_input_field)
-        val descriptionInput = findViewById<EditText>(R.id.description_input_field)
-        val dateInput = findViewById<EditText>(R.id.date_input_field)
-        //Disable keyboard in dateInput
-        dateInput.keyListener = null
+        //Disable keyboard when pressing on the date input field
+        binding.dateInputField.keyListener = null
 
-        //Date and Time picker dialog
-        val currentDate = Calendar.getInstance()
-        val year = currentDate.get(Calendar.YEAR)
-        val month = currentDate.get(Calendar.MONTH)
-        val day = currentDate.get(Calendar.DAY_OF_MONTH)
-        val datePickerDialog = DatePickerDialog(this, { _, year, month, dayOfMonth ->
-            //Create a new TimePickerDialog to choose the time
-            val currentTime = Calendar.getInstance() // Get the current time
-            val hour = currentTime.get(Calendar.HOUR_OF_DAY)
-            val minute = currentTime.get(Calendar.MINUTE)
-            val timePickerDialog = TimePickerDialog(this, { _, hourOfDay, minute ->
-                // Combine the selected date and time into a single Date object
-                val selectedDate = Calendar.getInstance()
-                selectedDate.set(year, month, dayOfMonth, hourOfDay, minute)
-                val selectedTimestamp = Timestamp(selectedDate.time)
-
-                // Set the chosen date and time in the EditText
-                dateInput.setText(selectedTimestamp.toDate().toString())
-                dateInput.tag = selectedTimestamp
-            }, hour, minute, true)
-
-            // Show the TimePickerDialog
-            timePickerDialog.show()
-        }, year, month, day)
-
-        // Show the DatePickerDialog when the dateInput field is clicked
-        dateInput.setOnClickListener {
-            datePickerDialog.show()
+        // Show the both date and time picker dialogs when pressing date input field
+        binding.dateInputField.setOnClickListener {
+            showDateTimePicker()
         }
 
         //Add button functionality
-        addButton.setOnClickListener {
-            // Input fields
-            val isPlus = plusMinusSwitch.isChecked
-            val amount = amountInput.text.toString().toDoubleOrNull() ?: 0.0
-            val description = descriptionInput.text.toString()
-            val date = if (dateInput.tag != null) dateInput.tag as Timestamp else Timestamp.now()
-
-            // Input fields layouts
-            val inputLayouts = listOf(
-                findViewById<TextInputLayout>(R.id.amount_input_layout),
-                findViewById<TextInputLayout>(R.id.description_input_layout),
-                findViewById<TextInputLayout>(R.id.date_input_layout)
-            )
-
-            // Check for empty fields and show error messages
-            inputLayouts.forEach { layout ->
-                layout.isErrorEnabled = layout.editText?.text.isNullOrBlank()
-                if (layout.isErrorEnabled) {
-                    layout.error = when (layout.id) {
-                        R.id.amount_input_layout -> "Fill in the amount"
-                        R.id.description_input_layout -> "Fill in the description"
-                        R.id.date_input_layout -> "Choose a date"
-                        else -> ""
-                    }
-                }
-            }
-
-            // If all fields are filled, add data to fire store
-            if (!inputLayouts.any { it.isErrorEnabled }) {
-                financeCollectionRef.add(MyFinanceModal(isPlus, amount, description, date))
-                    .addOnSuccessListener {
-                        //Success message in Logcat
-                        Log.i("Fire Store", "Finance card data has been added to fire store.")
-                        // Navigate back home with a success message extra (to main activity)
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.putExtra("message", "Finance card added successfully!")
-                        startActivity(intent)
-                        finish()
-                    }
-                    .addOnFailureListener {
-                        //Show error message in Snack-bar and in Logcat
-                        Log.d("Fire Store", "Error Adding Finance card: ", it)
-                        Snackbar.make(
-                            findViewById(android.R.id.content),
-                            "Error adding finance card: ${it.message}",
-                            Snackbar.LENGTH_SHORT
-                        ).show()
-                    }
-            }
+        binding.addButton.setOnClickListener {
+            addFinanceData()
         }
     }
 
@@ -134,5 +54,95 @@ class FinanceFormActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    //Add a new Finance card to the database function
+    private fun addFinanceData() {
+        // Input fields
+        val isPlus = binding.plusMinusSwitch.isChecked
+        val amount = binding.amountInputField.text.toString().toDoubleOrNull() ?: 0.0
+        val description = binding.descriptionInputField.text.toString()
+        val date = binding.dateInputField.tag as? Timestamp ?: Timestamp.now()
+
+        // Input fields layouts
+        val inputLayouts = listOf(
+            binding.amountInputLayout,
+            binding.descriptionInputLayout,
+            binding.dateInputLayout,
+        )
+
+        // Check for empty fields and show error messages
+        inputLayouts.forEach { layout ->
+            layout.isErrorEnabled = layout.editText?.text.isNullOrBlank()
+            if (layout.isErrorEnabled) {
+                layout.error = when (layout.id) {
+                    R.id.amount_input_layout -> "Fill in the amount"
+                    R.id.description_input_layout -> "Fill in the description"
+                    R.id.date_input_layout -> "Choose a date"
+                    else -> ""
+                }
+            }
+        }
+
+        // If all fields are filled, add data to fire store
+        if (!inputLayouts.any { it.isErrorEnabled }) {
+            financeCollectionRef.add(MyFinanceModal(isPlus, amount, description, date))
+                .addOnSuccessListener {
+                    //Success message in Logcat
+                    Log.i("Fire Store", "Finance card data has been added to fire store.")
+                    // Navigate back home with a success message extra (to main activity)
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("message", "Finance card added successfully!")
+                    startActivity(intent)
+                    finish()
+                }
+                .addOnFailureListener {
+                    //Show error message in Snack-bar and in Logcat
+                    Log.d("Fire Store", "Error Adding Finance card: ", it)
+                    Snackbar.make(
+                        binding.root,
+                        "Error adding finance card: ${it.message}",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+        }
+    }
+
+    //Show date and time picker dialogs
+    private fun showDateTimePicker() {
+        val currentTime = Calendar.getInstance()
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                val timePickerDialog = TimePickerDialog(
+                    this,
+                    { _, hourOfDay, minute ->
+                        val selectedDate = Calendar.getInstance().apply {
+                            set(year, month, dayOfMonth, hourOfDay, minute)
+                        }
+                        val selectedTimestamp = Timestamp(selectedDate.time)
+
+                        binding.dateInputField.setText(selectedTimestamp.toDate().toString())
+                        binding.dateInputField.tag = selectedTimestamp
+                    },
+                    currentTime.get(Calendar.HOUR_OF_DAY),
+                    currentTime.get(Calendar.MINUTE),
+                    true
+                )
+                timePickerDialog.show()
+            },
+            currentTime.get(Calendar.YEAR),
+            currentTime.get(Calendar.MONTH),
+            currentTime.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+    //Setup the actionBar function
+    private fun setupActionBar() {
+        //Enable the back button in the ActionBar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //Set the ActionBar title
+        supportActionBar?.title = "Add Financial Amount"
     }
 }
